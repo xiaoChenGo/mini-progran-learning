92d3d8940ef3b00b4b957c3dc261dd8d39480b37
xiaoChenGo <401163113@qq.com>
2022-12-03 00:09:45 +0800

    增加了 api 的编写

---
+++ pages/index/head/homeHead.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="content">
+		<!-- 标题 +搜索框 -->
+		<view >
+			<view class="u-navbar u-navbar-fixed" >
+				<!-- <view class="u-status-bar" ></view> -->
+				<view class="u-navbar-inner" >
+					<view class="u-back-wrap">
+						<view class="u-navbar-title">{{title||''}}</view>
+						<!-- u-icon-wrap u-back-text u-line-1 -->
+					</view>
+					<view 
+					hover-class="hover-search" 
+					class="search-box"
+					>
+					<!-- search-box u-flex u-row-center u-col-center u-m-r-30 -->
+						<u-icon name="search" color="red" size="66rpx"></u-icon>
+					</view>
+				</view>
+			</view>
+			<view class="u-swiper">
+				轮播图	
+			</view>			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '小程序demo'
+			};
+		},
+		onLoad() {},
+		methods: {},
+	};
+</script>
+
+<style lang="scss">
+	.u-navbar-inner{
+		width: 100%;
+		border: 1px solid;
+		display: flex;
+		justify-content: space-between;	
+		.u-navbar-title{
+			color: gray;
+			padding: 14rpx 14rpx 14rpx 24rpx;
+		}
+		.search-box {
+			width: 66rpx;
+			height: 66rpx;
+			background:#716d6d33;
+			border-radius: 100%;
+		}
+	}
+	
+
+	
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,26 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view>
+		<home-head></home-head>
 	</view>
 </template>
 
 <script>
+	import homeHead from '@/pages/index/head/homeHead.vue'
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
-		}
+			
+		},
+		components:{
+			homeHead,
+		},
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
